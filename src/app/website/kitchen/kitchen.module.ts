1e5402b22bbfd69454c26ea3f124ea4dbd65b210
Oriana <oriana.nohemi22@gmail.com>
2020-10-14 16:33:43 -0500

    traer pedidos desde la base de datos

--- src/app/website/kitchen/kitchen.module.ts
+++ src/app/website/kitchen/kitchen.module.ts
@@ -4,9 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { KitchenComponent } from './kitchen/kitchen.component';
 import { KitchenRoutingModule } from './kitchen-routing.module';
 import { SharedModule } from '../../shared/shared.module';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
-  declarations: [KitchenComponent],
+  declarations: [KitchenComponent, OrderComponent],
   imports: [KitchenRoutingModule, CommonModule, FormsModule, SharedModule],
 })
 export class KitchenModule {}
--- src/app/website/menu/product/product.component.ts
+++ src/app/website/menu/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../../../core/definitions/product.model';
 
-import { CartService } from '../../../core/services/cart.service'
+import { CartService } from '../../../core/services/cart.service';
 
 @Component({
   selector: 'app-product',
